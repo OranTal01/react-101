6cb36ace0febb87c966cc2345e4e3a4c6e363ec7
Oran Tal <orantal@Orans-MacBook-Air.local>
2021-04-26 17:27:56 +0300

    create cart item

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,16 +1,34 @@
+//dependencies
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
+//style
 import './cart-dropdown.style.scss';
 
+//components
 import CustomButton from '../custom-button/CustomButton';
+import CartItem from '../cart-item/CartItem';
 
-const CartDropdown = () => {
+const CartDropdown = ({ items }) => {
   return (
     <div className='cart-dropdown'>
-      <div className='card-items'></div>
-      <CustomButton type='button'>GO TO CHECKOUT</CustomButton>
+      <div className='cart-items'>
+        {items.map(item => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+CartDropdown.propTypes = {
+  items: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = ({ cart: { items } }) => ({
+  items,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cart-item/CartItem.jsx
@@ -0,0 +1,26 @@
+//dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//style
+import './cart-item.style.scss';
+
+const CartItem = ({ item: { imageUrl, name, price, quantity } }) => {
+  return (
+    <div className='cart-item'>
+      <img src={imageUrl} alt='item' />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+CartItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+
+export default CartItem;
